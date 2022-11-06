31b43b76f17aff8fe321e934bf2bcf74437a8640
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-06 21:35:16 +0100

    Add dishes table to menu page

--- src/menu.js
+++ src/menu.js
@@ -3,4 +3,62 @@ content.innerHTML = ''
 const header = document.createElement('h1')
 header.textContent = "Welcome To Complex Menu"
 content.append(header)
+content.innerHTML += `
+<table>
+<caption>Our Exotic Dishes</caption>
+<thead>
+    <tr>
+        <th>Dish Name</th>
+        <th>About</th>
+        <th>Taste</th>
+        <th>Found</th>
+        <th>Rating</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <th>Pizza</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th>Grand spicy chicken</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th>Nuggets</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th>Lasagne</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th>Waffles</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th>Beetroot</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</tbody>
+</table>
+`
 }
