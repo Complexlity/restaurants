3370a672bb6e9920b8e13df24622f85d8f279355
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-06 23:42:32 +0100

    Updated table header font weights

--- src/about.js
+++ src/about.js
@@ -3,6 +3,6 @@ content.innerHTML = `
 <section id="welcome-section" class="flex flex-center full-height">
         <h1>I am Complexlity</h1>
         <p>A Web Developer currently on the Odin Project Curriculum
-        <br>Click the Github Icon to see my codes and projects. Thank You</p>
+        <br>Click the <a class="go-to" target="_blank" href="https://github.com/Complexlity">Github</a> Icon to see my codes and projects. Thank You</p>
     </section>`
 }
