a9ec86e8b84f381414d507e4d3437862d07e7a27
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-06 23:54:44 +0100

    Remove unused config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-        // clean:true, 
         assetModuleFilename:'[name][ext]'
     },
     module: {
