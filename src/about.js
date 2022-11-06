17921634b7824820cad64edb42cbdc739c5eaa74
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-06 22:49:49 +0100

    Update hero styles

--- src/about.js
+++ src/about.js
@@ -2,7 +2,7 @@ export default function myPage(content){
 content.innerHTML = `
 <section id="welcome-section" class="flex flex-center full-height">
         <h1>I am Complexlity</h1>
-        <p>An Web Developer currently on the Odin Project Curriculum
+        <p>A Web Developer currently on the Odin Project Curriculum
         <br>Click the Github Icon to see my codes and projects. Thank You</p>
     </section>`
 }
