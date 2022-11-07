540bc09e522b511702f68b63a9242206a0814b8d
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-07 02:17:56 +0100

    Add Odin Project link

--- src/about.js
+++ src/about.js
@@ -2,7 +2,7 @@ export default function myPage(content){
 content.innerHTML = `
 <section id="welcome-section" class="flex flex-center full-height">
         <h1>I am Complexlity</h1>
-        <p>A Web Developer currently on the Odin Project Curriculum
+        <p>A Web Developer currently learning with <a class='go-to' target='_blank' href="https://www.theodinproject.com/">The Odin Project </a> FullStack JavaScript Curriculum
         <br>Go to <a class="go-to" target="_blank" href="https://github.com/Complexlity">Github</a> Icon to see my codes and projects. Thank You</p>
     </section>`
 }
